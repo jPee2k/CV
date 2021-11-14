49eebc6d1474ac6112b831ffb1b105564b865538
Serhii Pylypenko <jpee.dev@gmail.com>
2021-11-14 11:25:18 +0200

    add body coverage

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,9 +1,13 @@
+const body = document.querySelector('.page');
+const menuButton = body.querySelector('.main-nav__toggle');
+
 export const closeMenu = (button) => {
   button.setAttribute('aria-expanded', 'false');
   button.setAttribute('aria-label', 'открыть меню');
 
   const menu = button.parentElement;
   menu.classList.remove('main-nav--shown');
+  body.classList.remove('page--lock');
 };
 
 export const openMenu = (button) => {
@@ -12,6 +16,7 @@ export const openMenu = (button) => {
 
   const menu = button.parentElement;
   menu.classList.add('main-nav--shown');
+  body.classList.add('page--lock');
 };
 
 const mainMenuButton = document.querySelector('.main-nav__toggle');
@@ -20,5 +25,17 @@ mainMenuButton.addEventListener('click', (evt) => {
     closeMenu(evt.currentTarget);
   } else {
     openMenu(evt.currentTarget);
+  }
+});
+
+window.addEventListener('keyup', (evt) => {
+  if (evt.code === 'Escape') {
+    closeMenu(menuButton);
+  }
+});
+
+body.addEventListener('click', (evt) => {
+  if (evt.target.classList.contains('page--lock')) {
+    closeMenu(menuButton);
   }
 });
